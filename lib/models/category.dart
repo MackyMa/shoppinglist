518237563c0fbaa4c096f57d44df4163054bfa97
MackyMa <malaika.m99@gmail.com>
2025-11-20 12:00:13 -0500

    Made grocery item and category model

---
+++ lib/models/category.dart
@@ -0,0 +1,24 @@
+//enum categories
+import 'package:flutter/material.dart';
+
+enum Categories{
+  vegetables, 
+  fruit, 
+  meat, 
+  dairy, 
+  carbs, 
+  sweets, 
+  spices, 
+  convenience, 
+  hygiene, 
+  other,
+  }
+
+
+
+//class categories
+class Category {
+    const Category(this.title,this.color);
+    final String title;
+    final Color color;
+}
---
+++ lib/models/groceryitem.dart
@@ -0,0 +1,16 @@
+//just need grocery item def, not a widget just data 
+
+import 'package:shoppinglist/models/category.dart';
+
+class GroceryItem {
+  const GroceryItem ({
+    required this.id, 
+    required this.name, 
+    required this.quantity,
+    required this.category,
+  });
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
