cadb756eb0c6a88196ca5552eaec4c0a82a847a3
MackyMa <malaika.m99@gmail.com>
2025-12-19 22:48:11 -0500

    ensure there was a little spinner while sending and multiple sends disabled

--- lib/widgtes/new_item.dart
+++ lib/widgtes/new_item.dart
@@ -19,10 +19,14 @@ class _NewItemState extends State<NewItem>{
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async{
     if (_formKey.currentState!.validate()){
     _formKey.currentState!.save();
+    setState(() {
+      _isSending = true;
+    });
     //save to database 
     final url = Uri.https('shopping-list-93998-default-rtdb.firebaseio.com','shopping-list.json');
     final response = await http.post(url,
@@ -128,15 +132,22 @@ class _NewItemState extends State<NewItem>{
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                 TextButton(
-                  onPressed: () {
+                  onPressed:_isSending ? null : () {
                   _formKey.currentState!.reset();
                 }, 
                 child: Text("Reset")),
-                ElevatedButton(onPressed: _saveItem, child: Text ("Add Item")),
-              ],
+                ElevatedButton(
+                  onPressed:_isSending ? null : _saveItem,
+                  child: _isSending ? SizedBox(
+                    height: 16,
+                    width: 16,
+                    child: CircularProgressIndicator(),
+                  ) : Text("Add Item"),
               )
             ],
-          )
+          ),
+          ],
+          ),
         )
       )
     );
